48ac603105a8aa0b8c6d9536459c0fdc8eb518b4
Toren Darby <darby@science.ubc.ca>
2022-04-18 01:11:55 -0700

    Initial commit

---
+++ src/layouts/PopupLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout>
+    <!--    view="lHh Lpr lFf">-->
+    <q-header elevated>
+
+      Title
+
+    </q-header>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
