51e0af2f99aa1f10b8e11397fb838ebfa82b6cce
Toren Darby <torendarby@gmail.com>
2022-04-19 16:30:55 -0700

    Replace index with app skeleton

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,51 +1,51 @@
 <template>
-<!--  <q-page class="row items-center justify-evenly">-->
-  <div style="height: 500px; width: 500px;">
-  <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <!--  <q-page class="row items-center justify-evenly">-->
+  <div style="height: 500px; width: 500px">
+    <div v-if="currentAction.name === WORKING.name">
+      <working-control-bar
+        :session-duration-minutes="currentSessionDurationMinutes"
+      ></working-control-bar>
+    </div>
+    <stored-rest-bar :stored-rest-minutes="storedRestMinutes"></stored-rest-bar>
   </div>
-<!--  </q-page>-->
+  <!--  </q-page>-->
 </template>
 
-<script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { defineComponent, ref } from 'vue';
+<script setup lang="ts">
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import WorkingControlBar from 'components/WorkingControlBar.vue';
+import StoredRestBar from 'components/StoredRestBar.vue';
+import { ref } from 'vue';
 
-export default defineComponent({
-  name: 'IndexPage',
-  components: { ExampleComponent },
-  setup () {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
-});
+const currentSessionDurationMinutes = ref(0);
+const storedRestMinutes = ref(0);
+const totalWorkMinutes = ref(0);
+
+const WORKING = {
+  name: 'working',
+  onTick: () => {
+    currentSessionDurationMinutes.value += 1;
+    storedRestMinutes.value += 1 / 3;
+    totalWorkMinutes.value += 1;
+  },
+};
+const RESTING = {
+  name: 'resting',
+  onTick: () => {
+    storedRestMinutes.value -= 1;
+  },
+};
+const STOPPED = {
+  name: 'stopped',
+  onTick: () => {
+    // decrease to minimum of 0
+    storedRestMinutes.value = Math.max(0, storedRestMinutes.value - 1);
+  },
+};
+
+const currentAction = ref(WORKING);
+
+const workingTick = setInterval(() => {
+  currentAction.value.onTick();
+}, 1000); // Todo add `60 * ` to make it minutes, not seconds
 </script>
