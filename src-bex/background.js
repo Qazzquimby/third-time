40afc97592ea1c3dad1b3dd9fa3aaaa30c33af98
Toren Darby <darby@science.ubc.ca>
2022-05-03 11:23:42 -0700

    Move timer functionality to background.js

--- src-bex/background.js
+++ src-bex/background.js
@@ -1,3 +1,42 @@
+let timerRunning = false;
+let currentSessionDurationMinutes = 0;
+let storedRestMinutes = 0;
+let totalWorkMinutes = 0;
+
+const WORKING = {
+  name: 'working',
+  onTick: () => {
+    currentSessionDurationMinutes += 1;
+    storedRestMinutes += 1 / 3;
+    totalWorkMinutes += 1;
+  }
+};
+const RESTING = {
+  name: 'resting',
+  onTick: () => {
+    storedRestMinutes -= 1;
+  }
+};
+const STOPPED = {
+  name: 'stopped',
+  onTick: () => {
+    // decrease to minimum of 0
+    storedRestMinutes = Math.max(0, storedRestMinutes - 1);
+  }
+};
+
+let currentAction = WORKING;
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.cmd === 'TIMER_START') {
+//     currentSessionDurationMinutes.value = 0;
+//     currentAction.value = WORKING;
+//   } else if (request.cmd === 'TIMER_PAUSE') {
+//     currentAction.value = RESTING;
+//   } else if (request.cmd === 'TIMER_STOP') {
+//     currentAction.value = STOPPED;
+//   }
+// });
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.onClicked.addListener((/* tab */) => {
@@ -7,43 +46,67 @@ chrome.runtime.onInstalled.addListener(() => {
       url: chrome.runtime.getURL('www/index.html')
     }, (/* newTab */) => {
       // Tab opened.
-    })
-  })
-})
+    });
+  });
+});
 
-export default function (bridge /* , allActiveConnections */) {
+export default function(bridge /* , allActiveConnections */) {
   bridge.on('storage.get', event => {
-    const payload = event.data
+    const payload = event.data;
     if (payload.key === null) {
       chrome.storage.local.get(null, r => {
-        const result = []
+        const result = [];
 
         // Group the items up into an array to take advantage of the bridge's chunk splitting.
         for (const itemKey in r) {
-          result.push(r[itemKey])
+          result.push(r[itemKey]);
         }
-        bridge.send(event.eventResponseKey, result)
-      })
+        bridge.send(event.eventResponseKey, result);
+      });
     } else {
       chrome.storage.local.get([payload.key], r => {
-        bridge.send(event.eventResponseKey, r[payload.key])
-      })
+        bridge.send(event.eventResponseKey, r[payload.key]);
+      });
     }
-  })
+  });
 
   bridge.on('storage.set', event => {
-    const payload = event.data
+    const payload = event.data;
     chrome.storage.local.set({ [payload.key]: payload.data }, () => {
-      bridge.send(event.eventResponseKey, payload.data)
-    })
-  })
+      bridge.send(event.eventResponseKey, payload.data);
+    });
+  });
 
   bridge.on('storage.remove', event => {
-    const payload = event.data
+    const payload = event.data;
     chrome.storage.local.remove(payload.key, () => {
-      bridge.send(event.eventResponseKey, payload.data)
-    })
-  })
+      bridge.send(event.eventResponseKey, payload.data);
+    });
+  });
+
+  bridge.on('TIMER_START', () => {
+    currentSessionDurationMinutes.value = 0;
+    currentAction.value = WORKING;
+  });
+  bridge.on('TIMER_PAUSE', () => {
+    currentAction.value = RESTING;
+  });
+  bridge.on('TIMER_STOP', () => {
+    currentAction.value = STOPPED;
+  });
+
+  if (!timerRunning) {
+    timerRunning = true;
+    setInterval(() => {
+      currentAction.onTick();
+      bridge.send('ON_TICK_TIMERS', {
+        currentSessionDurationMinutes,
+        storedRestMinutes,
+        totalWorkMinutes,
+      })
+    }, 1000*60);
+  }
+
 
   /*
   // EXAMPLES
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,14 @@
 <template>
   <!--  <q-page class="row items-center justify-evenly">-->
   <div style="height: 500px; width: 500px">
-    <div v-if="currentAction.name === WORKING.name">
+    <div v-if="currentAction.name === 'working'">
       <working-control-bar
         :session-duration-minutes="currentSessionDurationMinutes"
         @pause="pause"
         @stop="stop"
       ></working-control-bar>
     </div>
-    <div v-else-if="currentAction.name === RESTING.name">
+    <div v-else-if="currentAction.name === 'resting'">
       <resting-control-bar
         :stored-rest-minutes="storedRestMinutes"
         @start="start"
@@ -18,8 +18,8 @@
 
     <div
       v-if="
-        currentAction.name === WORKING.name ||
-        currentAction.name === STOPPED.name
+        currentAction.name === 'working' ||
+        currentAction.name === 'stopped'
       "
     >
       <stored-rest-bar
@@ -38,49 +38,42 @@ import StoredRestBar from 'components/StoredRestBar.vue';
 import { ref } from 'vue';
 import RestingControlBar from 'components/RestingControlBar.vue';
 import TotalWorkBar from 'components/TotalWorkBar.vue';
+import { useQuasar } from 'quasar';
 
-const currentSessionDurationMinutes = ref(0);
+const $q = useQuasar()
+
+const currentAction = {name: 'working'}
+const currentSessionDurationMinutes = ref(0)
 const storedRestMinutes = ref(0);
 const totalWorkMinutes = ref(0);
 
-const WORKING = {
-  name: 'working',
-  onTick: () => {
-    currentSessionDurationMinutes.value += 1;
-    storedRestMinutes.value += 1 / 3;
-    totalWorkMinutes.value += 1;
-  },
-};
-const RESTING = {
-  name: 'resting',
-  onTick: () => {
-    storedRestMinutes.value -= 1;
-  },
-};
-const STOPPED = {
-  name: 'stopped',
-  onTick: () => {
-    // decrease to minimum of 0
-    storedRestMinutes.value = Math.max(0, storedRestMinutes.value - 1);
-  },
-};
-
-function pause() {
-  currentAction.value = RESTING;
+function start() {
+  $q.bex.send('TIMER_START')
 }
 
-function start() {
-  currentSessionDurationMinutes.value = 0;
-  currentAction.value = WORKING;
+function pause() {
+  $q.bex.send('TIMER_PAUSE')
 }
 
 function stop() {
-  currentAction.value = STOPPED;
+  $q.bex.send('TIMER_STOP')
 }
 
-const currentAction = ref(WORKING);
+console.log($q);
 
-setInterval(() => {
-  currentAction.value.onTick();
-}, 1000); // Todo add `60 * ` to make it minutes, not seconds
+$q.bex.on('ON_TICK_TIMERS', (
+  response: { data: {
+    currentSessionDurationMinutes: number,
+    storedRestMinutes: number,
+    totalWorkMinutes: number
+    }
+  }
+) => {
+  const timers = response.data
+  console.log(timers);
+  currentSessionDurationMinutes.value = timers.currentSessionDurationMinutes;
+  storedRestMinutes.value = timers.storedRestMinutes;
+  totalWorkMinutes.value = timers.totalWorkMinutes;
+})
+
 </script>
