8b44e1ab339fd59509566fb296035e0125af08ac
Toren Darby <darby@science.ubc.ca>
2022-05-11 14:44:31 -0700

    Fix index to work with stored timerMode

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,31 +1,37 @@
 <template>
   <!--  <q-page class="row items-center justify-evenly">-->
   <div style="height: 500px; width: 500px">
-    <div v-if="currentAction.name === 'working'">
-      <working-control-bar
-        :session-duration-minutes="currentSessionDurationMinutes"
-        @pause="pause"
-        @stop="stop"
-      ></working-control-bar>
-    </div>
-    <div v-else-if="currentAction.name === 'resting'">
-      <resting-control-bar
-        :stored-rest-minutes="storedRestMinutes"
-        @start="start"
-        @stop="stop"
-      ></resting-control-bar>
-    </div>
+    <div v-if="initialized">
+      <div v-if="timerMode === 'working'">
+        <working-control-bar
+          :session-duration-minutes="currentSessionDurationMinutes"
+          @pause="pause"
+          @stop="stop"
+        ></working-control-bar>
+      </div>
+      <div v-else-if="timerMode === 'resting'">
+        <resting-control-bar
+          :stored-rest-minutes="storedRestMinutes"
+          @start="start"
+          @stop="stop"
+        ></resting-control-bar>
+      </div>
+      <div v-else-if="timerMode === 'stopped'">
+        <stopping-control-bar
+          :stored-rest-minutes="storedRestMinutes"
+          @start="start"
+          @reset="reset"
+        ></stopping-control-bar>
+      </div>
+      <div v-else>Invalid timerMode {{ timerMode }}</div>
 
-    <div
-      v-if="
-        currentAction.name === 'working' || currentAction.name === 'stopped'
-      "
-    >
-      <stored-rest-bar
-        :stored-rest-minutes="storedRestMinutes"
-      ></stored-rest-bar>
+      <div v-if="timerMode === 'working' || timerMode === 'stopped'">
+        <stored-rest-bar
+          :stored-rest-minutes="storedRestMinutes"
+        ></stored-rest-bar>
+      </div>
+      <total-work-bar :total-work-minutes="totalWorkMinutes"></total-work-bar>
     </div>
-    <total-work-bar :total-work-minutes="totalWorkMinutes"></total-work-bar>
   </div>
   <!--  </q-page>-->
 </template>
@@ -34,48 +40,63 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import WorkingControlBar from 'components/WorkingControlBar.vue';
 import StoredRestBar from 'components/StoredRestBar.vue';
-import { ref } from 'vue';
+import { onBeforeMount, ref } from 'vue';
 import RestingControlBar from 'components/RestingControlBar.vue';
 import TotalWorkBar from 'components/TotalWorkBar.vue';
 import { useQuasar } from 'quasar';
 
 const $q = useQuasar();
 
-const currentAction = { name: 'working' };
+const initialized = ref(false);
+const timerMode = ref('stopped');
 const currentSessionDurationMinutes = ref(0);
 const storedRestMinutes = ref(0);
 const totalWorkMinutes = ref(0);
 
-// @ts-ignore chrome is present but not found
-chrome.storage.local
-  .get([
-    'currentSessionDurationMinutes',
-    'storedRestMinutes',
-    'totalWorkMinutes',
-  ])
-  .then(
-    (timers: {
-      currentSessionDurationMinutes: number;
-      storedRestMinutes: number;
-      totalWorkMinutes: number;
-    }) => {
-      currentSessionDurationMinutes.value =
-        timers.currentSessionDurationMinutes;
-      storedRestMinutes.value = timers.storedRestMinutes;
-      totalWorkMinutes.value = timers.totalWorkMinutes;
-    }
-  );
+onBeforeMount(() => {
+  // @ts-ignore chrome is present but not found
+  chrome.storage.local
+    .get([
+      'timerMode',
+      'currentSessionDurationMinutes',
+      'storedRestMinutes',
+      'totalWorkMinutes',
+    ])
+    .then(
+      (resp: {
+        timerMode: string;
+        currentSessionDurationMinutes: number;
+        storedRestMinutes: number;
+        totalWorkMinutes: number;
+      }) => {
+        initialized.value = true;
+        timerMode.value = resp.timerMode;
+        currentSessionDurationMinutes.value =
+          resp.currentSessionDurationMinutes;
+        storedRestMinutes.value = resp.storedRestMinutes;
+        totalWorkMinutes.value = resp.totalWorkMinutes;
+      }
+    );
+});
 
 function start() {
+  console.log('sending start');
   $q.bex.send('TIMER_START');
 }
 
 function pause() {
+  console.log('sending pause');
   $q.bex.send('TIMER_PAUSE');
 }
 
 function stop() {
+  console.log('sending stop');
   $q.bex.send('TIMER_STOP');
+}
+
+function reset() {
+  console.log('sending reset');
+  $q.bex.send('TIMER_RESET');
 }
 
 $q.bex.on(
