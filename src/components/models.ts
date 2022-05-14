50274ca2afb263aa6e05923c6bfdfff86dcb9f33
Toren Darby <darby@science.ubc.ca>
2022-05-14 00:24:20 -0700

    Add time formatting

--- src/components/models.ts
+++ src/components/models.ts
@@ -1,3 +1,5 @@
+import { Duration } from 'luxon';
+
 export interface Todo {
   id: number;
   content: string;
@@ -5,4 +7,15 @@ export interface Todo {
 
 export interface Meta {
   totalCount: number;
+}
+
+export function makeTimeString(seconds: number): string {
+  const duration = Duration.fromObject({ seconds: Math.floor(seconds) })
+    .shiftTo('hours', 'minutes', 'seconds')
+    .toObject();
+  if (duration.hours) {
+    return `${duration.hours}h ${duration.minutes}m ${duration.seconds}s`;
+  } else {
+    return `${duration.minutes}m ${duration.seconds}s`;
+  }
 }
