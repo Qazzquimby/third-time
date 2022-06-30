a163e23ec8fe336b5bb571fc87845fc116aac067
Toren Darby <darby@science.ubc.ca>
2022-06-30 14:25:45 -0700

    feat: style workbar

--- src/components/WorkBar.vue
+++ src/components/WorkBar.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-import { currentSessionDurationSeconds } from '~/composables/state'
+import { currentSessionDurationSeconds, isWorking } from '~/composables/state'
+
+const barClasses = computed(() => {
+  if (isWorking()) {
+    return ['h-15', 'text-3xl']
+  }
+  return 'h-0 op-0'
+})
 </script>
 
 <template>
-  <div flex="~ row grow" gap-2 w-full>
-    <span flex-basis-100 text-right font-mono>{{ formatTime(currentSessionDurationSeconds) }}</span>
-    <span flex-basis-100>Worked</span>
+  <div transition-all-1000 :class="barClasses">
+    <div flex="~ row grow" gap-2 w-full>
+      <span flex-basis-100 text-right font-mono>{{ formatTime(currentSessionDurationSeconds) }}</span>
+      <span flex-basis-100>Worked</span>
+    </div>
   </div>
 </template>
