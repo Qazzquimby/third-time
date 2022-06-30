ceec442677035ff1cf79134862889e63407735ff
Toren Darby <darby@science.ubc.ca>
2022-06-30 14:46:52 -0700

    feat: add growing to rest bar

--- src/components/RestBar.vue
+++ src/components/RestBar.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import { storedRestSeconds } from '~/composables/state'
+import { isResting, storedRestSeconds } from '~/composables/state'
+
+const barClasses = computed(() => {
+  if (isResting()) {
+    return ['h-15', 'text-4xl']
+  }
+})
 </script>
 
 <template>
-  <div text-2xl>
+  <div transition-all-1000 text-2xl :class="barClasses">
     <div flex="~ row grow" gap-2 w-full>
       <span flex-basis-100 text-right font-mono>{{ formatTime(storedRestSeconds) }}</span>
       <span flex-basis-100>Rest</span>
