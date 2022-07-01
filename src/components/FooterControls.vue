4b31edf922e1a8e0b77f248340e7af8725043cb3
Toren Darby <torendarby@gmail.com>
2022-07-01 13:52:43 -0700

    add reset button

--- src/components/FooterControls.vue
+++ src/components/FooterControls.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
-import { minutesSinceModeChange, setMinutesSinceModeChange } from '~/composables/state'
+import { minutesSinceModeChange, reset, setMinutesSinceModeChange } from '~/composables/state'
+
+const resetButtonClasses = computed(() => {
+  if (isStopped()) {
+    return ['top-0']
+  }
+  return ['top-30 op-0']
+})
 </script>
 
 <template>
+  <div transition-all-1000 relative :class="resetButtonClasses">
+    <div flex="~ row" justify-center>
+      <div btn border-rd-100  @click="reset()">
+        <div i-mdi-refresh text-6xl />
+      </div>
+    </div>
+  </div>
+
   <!-- consider using https://headlessui.com/vue/dialog -->
   <div>
     <p>
@@ -15,7 +30,7 @@ import { minutesSinceModeChange, setMinutesSinceModeChange } from '~/composables
     </p>
   </div>
 
-  <ul flex="~ row" gap-3>
+  <ul flex="~ row" justify-between gap-3 mx-3>
     <li>
       <button btn @click="start">
         PLAY
