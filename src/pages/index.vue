ac44a9a51991f300eb1a2e24bdf61a83ae48d69f
Toren Darby <torendarby@gmail.com>
2022-07-03 10:41:50 -0700

    feat: add dynamic title showing timer value

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { backgroundClass } from '~/composables/state'
+import { backgroundClass, storage } from '~/composables/state'
 
 const audio = new Audio(
   'https://cdn.videvo.net/videvo_files/audio/premium/audio0151/watermarked/Ringtone-Alarm-Smart-Phone-Vibe-Chime-Alarm-or-Alert_COMM-1450_preview.mp3',
@@ -7,6 +7,18 @@ const audio = new Audio(
 watch(storedRestSeconds, (newSeconds, oldSeconds) => {
   if (oldSeconds >= 0 && newSeconds < 0) {
     audio.play()
+  }
+})
+
+watch([storage.value.timerMode, storedRestSeconds, currentSessionDurationSeconds], () => {
+  if (isWorking.value) {
+    document.title = `Working: ${formatTime(currentSessionDurationSeconds.value)}`
+  }
+  else if (isResting.value) {
+    document.title = `Resting: ${formatTime(storedRestSeconds.value)}`
+  }
+  else {
+    document.title = 'ThirdTime Stopped'
   }
 })
 
