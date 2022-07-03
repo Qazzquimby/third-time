cac59d388b4111ccbf8212c883c8c9913c20a9fb
Toren Darby <torendarby@gmail.com>
2022-07-02 22:20:20 -0700

    fix: barbox styling

--- src/components/BarBox.vue
+++ src/components/BarBox.vue
@@ -20,20 +20,20 @@ const barDividerClasses = computed(() => {
     <work-bar z-0 />
     <div transition-all-1000 :class="[getBackgroundClass()]" relative z-2>
       <div
-        max-w-80 mx-auto py-3
+        max-w-100 mx-auto py-3
         bg-gradient-to-b from-white:25 to bg-white:10
         border="rd-10 solid 1px" border-white:30
         grid style="grid-template-rows: 1fr 5px 1fr" items-center
       >
-        <rest-bar style="grid-row: 1" z-3 />
+        <rest-bar z-3 />
         <div
           transition-all-1000
           relative left--2px
           :class="[getBackgroundClass(), barDividerClasses]"
-          style="grid-row: 2; width: 110%"
+          style="width: 110%"
           z-2
         />
-        <goal-bar style="grid-row: 3" />
+        <goal-bar />
       </div>
     </div>
   </div>
