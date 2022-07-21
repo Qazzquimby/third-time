b637150d48c82978cd9d96a6e18af1105f91c3d3
Toren Darby <torendarby@gmail.com>
2022-07-21 13:00:28 -0700

    feat: style header to match body width

--- src/components/TimerHeader.vue
+++ src/components/TimerHeader.vue
@@ -3,23 +3,29 @@ import { runTour } from '~/composables/tour'
 </script>
 
 <template>
-  <div
-    flex="~"
-    text="1.5rem"
-  >
-    <span
-      i-mdi-clock-time-nine-outline
-      inline-block
-    />
-    <h1>ThirdTime</h1>
-    <button
-      ml-auto
-      glass text-center font-bold
-      border-rd-100 h-10 w-10
-      @click="runTour"
+  <div flex justify-center>
+    <div
+      flex="~"
+      text="1.5rem"
+      max-w-100
+      w-full
     >
-      ?
-    </button>
+      <span
+        i-mdi-clock-time-nine-outline
+        inline-block
+      />
+      <h1>ThirdTime</h1>
+
+      <div flex-grow />
+
+      <button
+        glass text-center font-bold
+        border-rd-100 h-10 w-10
+        @click="runTour"
+      >
+        ?
+      </button>
+    </div>
   </div>
 </template>
 
