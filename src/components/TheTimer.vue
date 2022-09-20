b98d52a9b3052dc0a5ab060c62a3d523e77d6b08
Toren Darby <torendarby@gmail.com>
2022-09-19 19:04:16 -0700

    fix: use innerheight to fit on phone screens

--- src/components/TheTimer.vue
+++ src/components/TheTimer.vue
@@ -1,10 +1,15 @@
+<script setup lang="ts">
+const { height } = useWindowSize()
+</script>
+
 <template>
   <div
     p="x-4 t-4" text-white
-    h-screen
+    h-screen_COMMENTEDOUT
     flex="~ col" justify-between
     transition-1000
     :class="[backgroundClass]"
+    :style="`height: ${height}px`"
     overflow-clip
     style="scroll-snap-align: start"
   >
